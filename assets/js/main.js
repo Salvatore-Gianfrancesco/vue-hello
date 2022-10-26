eeca9bf7434ed982b122740caa706afa52ffae98
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-10-26 14:42:31 +0200

    added image and html-css layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,9 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: "hello."
+            message: "hello.",
+            img: "./assets/img/mac.webp",
+            h1Classes: "pt-3"
         }
     }
-}).mount("#app");
+}).mount('#app');
